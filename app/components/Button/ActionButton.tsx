97cadbfadabfed2c89fb7b19e81303192f5e3296
iusok3386 <ita.kosu55@gmail.com>
2024-11-10 07:54:12 +0000

    add: ActionButton を追加

---
+++ app/components/Button/ActionButton.tsx
@@ -0,0 +1,45 @@
+import { Button, ButtonProps } from "@/components/ui/button"
+import { MaterialSymbols } from "../Icon/MaterialSymbols"
+import React from "react";
+
+/** 
+ * @param iconName Material Symbols のアイコン名  
+ * @param iconUrl  アイコンファイルの URL
+ * 
+ * @remarks
+ * `iconName`と`iconUrl`を同時に指定した場合の動作はサポートしません
+ */
+export function ActionButton({
+    iconName,
+    iconUrl,
+    children,
+
+    // components/ui/button.tsx を参考に
+    className,
+    variant,
+    size,
+    asChild = false,
+    ...props
+}: {
+    iconName?: string,
+    iconUrl?:  string,
+    children?: React.ReactNode;
+} & ButtonProps) {
+    return (
+        <Button className={`flex gap-2 pl-4 pr-6 py-[10px] rounded-full ${className}`}
+                variant={variant}
+                size={size}
+                asChild={asChild}
+                {...props}>
+            {
+                iconName ? <MaterialSymbols size={18}>add</MaterialSymbols> : null
+            }
+            {
+                iconUrl ? <img src={iconUrl} /> : null
+            }
+            {
+                children
+            }
+        </Button>
+    )
+}
