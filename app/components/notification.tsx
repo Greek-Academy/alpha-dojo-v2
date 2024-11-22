0b5fae6183a8972e2cfde38f32683475a8b51aa4
iusok3386 <ita.kosu55@gmail.com>
2024-11-22 14:45:22 +0000

    add: ざっくり通知コンポーネントを作った add: react-markdown を導入した (が、npm で force したので不具合があるかも)

---
+++ app/components/notification.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image"
+import Link from "next/link"
+import Markdown from 'react-markdown'
+
+// 要件が決まったら、ユーザー情報取得ライブラリを作成
+function getUserInfo(userId: string) {
+    return {
+        name: "sumiredc",
+        icon: "",
+        link: "",
+    }
+}
+
+interface NotificataionProps {
+    userId:  string,
+    content: string,
+}
+
+export const Notification = ({
+    notifications
+}: {
+    notifications: NotificataionProps[]
+}) => {
+    return (
+        <div className="flex flex-col gap-2 w-fit h-fit p-4 rounded-xl bg-slate-100 border-border border-2">
+            <h1 className="text-xl font-medium">Notifications</h1>
+            {
+                notifications.map((notification) => {
+                    const user = getUserInfo(notification.userId)
+                    return (
+                        <div className="flex gap-2 w-80 p-3 bg-slate-200">
+                            {/* 実際に使う際には、信用する URL を next.config.ts に追加する必要がある */}
+                            <Link
+                                href={user.link}
+                            >
+                                <Image
+                                    key={`user-${notification.userId}`}
+                                    src={user.icon}
+                                    width={24}
+                                    height={24}
+                                    fit
+                                    alt={`${user.name} さん`}
+                                />
+                            </Link>
+                            <Markdown>{notification.content}</Markdown>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
