de8f84e1317a630eb123ab3708221bc59590364f
iusok3386 <ita.kosu55@gmail.com>
2024-11-30 15:26:21 +0000

    chore: lint に修正しろと言われたものを修正 (未使用の import を削除)

--- app/components/code-tab.tsx
+++ app/components/code-tab.tsx
@@ -1,27 +1,27 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ClipboardPenLine, Code, FileText, FlaskConical } from "lucide-react";
+import { Card, CardContent } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Code } from 'lucide-react';
 
 export const CodeTab = () => {
-    return (
-        <Card className="w-full h-full">
-            <Tabs defaultValue="code" className="w-full h-full">
-                <TabsList className="grid w-full grid-cols-3">
-                    <TabsTrigger value="code" className="w-full"><Code size="14"/>code</TabsTrigger>
-                </TabsList>
-                <div className="h-full">
-                    <TabsContent value="code" className="h-full bg-white">
-                        <Card className="h-full">
-                            
-                            <CardContent className="space-y-2">
-                                ここにコードを書きます。。
-                            </CardContent>
-                            
-                        </Card>
-                    </TabsContent>
-                    
-                </div>
-            </Tabs>
-        </Card>
-    );
+  return (
+    <Card className="w-full h-full">
+      <Tabs defaultValue="code" className="w-full h-full">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="code" className="w-full">
+            <Code size="14" />
+            code
+          </TabsTrigger>
+        </TabsList>
+        <div className="h-full">
+          <TabsContent value="code" className="h-full bg-white">
+            <Card className="h-full">
+              <CardContent className="space-y-2">
+                ここにコードを書きます。。
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </div>
+      </Tabs>
+    </Card>
+  );
 };
