cf248084e8c39158855fef5cc4db161f0598c590
iusok3386 <ita.kosu55@gmail.com>
2024-12-08 00:49:00 +0000

    fix: デザインの修正 (一時的なデザイン)

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,17 @@ export default function Home() {
             </ToggleGroup>
             <div>
                 <h1 className="text-xl">課題一覧がここに表示されます</h1>
-                <Link href="/submissions/1/edit">編集画面へ</Link>
+                <Link href="/submissions/1/edit" className="font-bold underline text-blue-600 visited:text-purple-600">編集画面へ</Link>
             </div>
         </div>
-        <div className="w-100">
-            <div>
+        <div className="w-100 flex flex-col gap-3">
+            <div className="border rounded-xl p-5 flex flex-col gap-2.5 bg-accent text-accent-foreground">
                 <h1 className="text-xl">Notifications</h1>
+                通知がここに表示されます
             </div>
-            <div>
+            <div className="border rounded-xl p-5 flex flex-col gap-2.5 bg-accent text-accent-foreground">
                 <h1 className="text-xl">Achievements</h1>
+                実績がここに表示されます
             </div>
         </div>
     </div>
