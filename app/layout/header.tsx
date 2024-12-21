2a95f392813770de164ad14d49ea8e3c7d4551fd
iusok3386 <ita.kosu55@gmail.com>
2024-12-21 15:14:08 +0000

    fix: Header のデザインを Figma に合わせた

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,30 +1,25 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
 import { CircleUserRound, Play, Upload } from 'lucide-react';
 
 export const Header = () => {
   return (
-    <div className="w-screen h-9 mt-2">
-      <Card>
-        <CardContent>
-          <div className="w-full flex justify-between items-center">
-            <div className="items-center">Alpha Dojo</div>
-            <div className="flex gap-2">
-              <Button variant="outline">
-                <Play />
-                Run
-              </Button>
-              <Button>
-                <Upload />
-                Submit
-              </Button>
-            </div>
-            <div className="items-center">
-              <CircleUserRound size="24" />
-            </div>
+    <div className="w-full px-6 py-2">
+        <div className="w-full flex justify-between items-center">
+          <div className="items-center">Alpha Dojo</div>
+          <div className="flex gap-2">
+            <Button variant="outline">
+              <Play />
+              Run
+            </Button>
+            <Button>
+              <Upload />
+              Submit
+            </Button>
           </div>
-        </CardContent>
-      </Card>
+          <div className="items-center">
+            <CircleUserRound size="24" />
+          </div>
+        </div>
     </div>
   );
 };
