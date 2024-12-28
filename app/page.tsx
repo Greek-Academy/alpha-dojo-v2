d9d4fb31de8410a46370a81980232d447710a0ea
iusok3386 <ita.kosu55@gmail.com>
2024-12-28 01:39:41 +0000

    improve: class-variance-authority を使って Split の ClassName の可読性を向上 feat: Splitter の区切り線を表示・アニメーション fix: Splitter の区切り部分をクリックするまでカーソルアイコンが変化しないのを修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 import { ProbremTab } from './components/problem-tab';
 import { TestTab } from './components/test-tab';
 import { CodeTab } from './components/code-tab';
-import Split from 'react-split';
+import Split, { SplitProps } from 'react-split';
+import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
 
 class Person {
   name: string = '';
@@ -23,18 +25,78 @@ samplePersons.push(new Person('cさん', 25, 'Nagoya'));
 samplePersons.push(new Person('dさん', 32, 'Osaka'));
 
 export default function Home() {
+  const splitVariants = cva(
+    'flex w-full h-full',
+    {
+      variants: {
+        direction: {
+          horizontal: 'flex-row',
+          vertical: 'flex-col',
+        },
+      },
+      defaultVariants: {
+        direction: 'horizontal',
+      },
+    }
+  );
+
+  const gutterVariants = cva(
+    'flex items-center justify-center group',
+    {
+      variants: {
+        direction: {
+          horizontal: 'hover:cursor-col-resize',
+          vertical: 'hover:cursor-row-resize',
+        },
+      },
+      defaultVariants: {
+        direction: 'horizontal',
+      },
+    }
+  );
+
+  const gutterChildVariants = cva(
+    'bg-border-variant opacity-0 group-hover:opacity-100 transition-[opacity,width,height]',
+    {
+      variants: {
+        direction: {
+          horizontal: 'w-1 h-full group-active:w-0.5',
+          vertical:   'h-1 w-full group-active:h-0.5',
+        },
+      },
+      defaultVariants: {
+        direction: 'horizontal',
+      },
+    }
+  );
+
+  const splitProps = (
+    direction: 'horizontal' | 'vertical' | undefined,
+    className?: string
+  ): SplitProps => {
+    return {
+      direction,
+      minSize: 200,
+      className: cn(splitVariants({ direction, className })),
+      gutterSize: 10,
+      gutter: () => {
+        const gutterElement = document.createElement('div');
+        gutterElement.className = gutterVariants({direction});
+
+        // 子要素 (区切り線の可視化・アニメーション)
+        const gutterChild = document.createElement('div');
+        gutterChild.className = gutterChildVariants({direction});
+        gutterElement.appendChild(gutterChild);
+
+        return gutterElement;
+      },
+    };
+  };
+
   return (
-    <Split
-      direction="horizontal"
-      minSize={200}
-      className="flex w-full h-full px-2.5 pb-2.5 min-w-100 min-h-100"
-    >
+    <Split {...splitProps('horizontal', 'px-2.5 pb-2.5 min-w-100 min-h-100')}>
       <ProbremTab className="overflow-auto" />
-      <Split
-        direction="vertical"
-        minSize={200}
-        className="flex w-full h-full flex-col"
-      >
+      <Split {...splitProps('vertical')}>
         <CodeTab className="overflow-auto" />
         <TestTab className="overflow-auto" />
       </Split>
