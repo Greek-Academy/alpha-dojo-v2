63ef73b91e34cee427fd8f41546c46a73cde23a9
iusok3386 <ita.kosu55@gmail.com>
2025-01-11 02:52:14 +0000

    chore: lint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -103,11 +103,11 @@ const config: Config = {
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
-        }
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       transitionDuration: {
         '400': '400ms',
