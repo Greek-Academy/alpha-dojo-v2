2e4416721a2c85b446583f4fe1f0b47d381452fe
iusok3386 <ita.kosu55@gmail.com>
2025-01-12 07:29:48 +0000

    chore: lint の再実行

--- app/components/problem-tab.tsx
+++ app/components/problem-tab.tsx
@@ -1,4 +1,9 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 import {
   Card,
   CardContent,
@@ -43,10 +48,14 @@ export const ProbremTab = ({ className }: { className?: string }) => {
                   Hint 1
                 </AccordionTrigger>
                 <AccordionContent>
-                  ヒント その1<br />
-                  ヒント その1<br />
-                  ヒント その1<br />
-                  ヒント その1<br />
+                  ヒント その1
+                  <br />
+                  ヒント その1
+                  <br />
+                  ヒント その1
+                  <br />
+                  ヒント その1
+                  <br />
                   ヒント その1
                 </AccordionContent>
               </AccordionItem>
@@ -56,10 +65,14 @@ export const ProbremTab = ({ className }: { className?: string }) => {
                   Hint 2
                 </AccordionTrigger>
                 <AccordionContent>
-                  ヒント その2<br />
-                  ヒント その2<br />
-                  ヒント その2<br />
-                  ヒント その2<br />
+                  ヒント その2
+                  <br />
+                  ヒント その2
+                  <br />
+                  ヒント その2
+                  <br />
+                  ヒント その2
+                  <br />
                   ヒント その2
                 </AccordionContent>
               </AccordionItem>
