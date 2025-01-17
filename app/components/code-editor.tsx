07dd153bc4bebec94e23c27c1474ef157bda8072
iusok3386 <ita.kosu55@gmail.com>
2025-01-17 14:40:00 +0000

    add: Monaco Editor の読み込み待機時に、Spinner を表示

--- app/components/code-editor.tsx
+++ app/components/code-editor.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Editor, { EditorProps } from '@monaco-editor/react';
 import { Language } from '@/lib/languages';
+import Image from 'next/image';
+import Spinner from '@/public/spinner.svg'
 
 export const CodeEditor = ({ language, ...props }: EditorProps) => {
   return (
-    <Editor language={language || Language.typescript.id.monaco} {...props} />
+    <Editor
+      language={language || Language.typescript.id.monaco}
+      loading={<Image src={Spinner} width={40} height={40} alt="loading" />}
+      {...props} />
   );
 };
