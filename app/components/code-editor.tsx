1fd62df2c4400f6babc74a2577d7c94d97b869ca
iusok3386 <ita.kosu55@gmail.com>
2025-01-17 14:28:56 +0000

    add: code-tab の言語切替時にデフォルトのコードに書き戻す仕様を追加

--- app/components/code-editor.tsx
+++ app/components/code-editor.tsx
@@ -2,33 +2,8 @@ import React from 'react';
 import Editor, { EditorProps } from '@monaco-editor/react';
 import { Language } from '@/lib/languages';
 
-const defaultValue = `class Solution {
-public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    int len = nums.size();
-    vector<int> res;
-    
-    for (int i = 0; i < len; i++) {
-      for (int j = i + 1; j < len; j++) {
-        if (nums[i] + nums[j] == target) {
-          res.push_back(i);
-          res.push_back(j);
-          return res;
-        }
-      }
-    }
-    
-    return res;
-  }
-};
-`;
-
 export const CodeEditor = ({ language, ...props }: EditorProps) => {
   return (
-    <Editor
-      defaultValue={defaultValue}
-      language={language || Language.typescript.id.monaco}
-      {...props}
-    />
+    <Editor language={language || Language.typescript.id.monaco} {...props} />
   );
 };
--- app/components/code-tab.tsx
+++ app/components/code-tab.tsx
@@ -12,11 +12,70 @@ import {
 } from '@/components/ui/select';
 import React from 'react';
 
+const defaultCodes: { [key in SupportedLanguage]: string } = {
+  typescript: String.raw`class Cat {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  meow(): void {
+    console.log("Meow!");
+  }
+}
+
+let myCat = new Cat("Tama", 5);
+console.log(myCat.name); // 出力: Tama
+myCat.meow(); // 出力: Meow!`,
+
+  python: String.raw`class Cat:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def meow(self):
+    print("Meow!")
+
+  my_cat = Cat("Tama", 5)
+  print(my_cat.name)  # 出力: Tama
+  my_cat.meow()  # 出力: Meow!`,
+
+  c: String.raw`#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+  char name[50];
+  int age;
+} Cat;
+
+void meow(Cat *cat) {
+  printf("Meow!\n");
+}
+
+int main() {
+  Cat my_cat;
+  strcpy(my_cat.name, "Tama");
+  my_cat.age = 5;
+
+  printf("%s\n", my_cat.name); // 出力: Tama
+  meow(&my_cat); // 出力: Meow!
+
+  return 0;
+}`,
+};
+
 export const CodeTab = ({ className }: { className?: string }) => {
   const defaultLanguage: SupportedLanguage = 'typescript';
 
   const [language, setLanguage] =
     React.useState<SupportedLanguage>(defaultLanguage);
+
+  const [codeValue, setCodeValue] = React.useState<string>(
+    defaultCodes[defaultLanguage]
+  );
 
   return (
     <Tabs defaultValue="code" className={cn('h-full', className)}>
@@ -34,6 +93,7 @@ export const CodeTab = ({ className }: { className?: string }) => {
               defaultValue={defaultLanguage}
               onValueChange={(value) => {
                 setLanguage(value as SupportedLanguage);
+                setCodeValue(defaultCodes[value as SupportedLanguage]);
               }}
             >
               <SelectTrigger className="w-auto border-0 shadow-none gap-2 p-0 h-auto">
@@ -51,7 +111,11 @@ export const CodeTab = ({ className }: { className?: string }) => {
             </Select>
           </div>
 
-          <CodeEditor language={Language[language].id.monaco} />
+          <CodeEditor
+            language={Language[language].id.monaco}
+            defaultValue={defaultCodes[defaultLanguage]}
+            value={codeValue}
+          />
         </div>
       </TabsContent>
     </Tabs>
