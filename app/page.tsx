45cdb304d8a4d993199314ecffaeb0fa1cf1b696
Y.Kobayashi <88024945+kobayashi020@users.noreply.github.com>
2025-01-30 01:31:10 +0900

    page.tsx →'Nortifications','Achievements'を太字表示に

--- app/page.tsx
+++ app/page.tsx
@@ -2,74 +2,10 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { AddIcon } from './components/icons/material-symbols';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
-import { Sample, SubmissionTable } from './components/submission-table';
+import { SubmissionTable } from './components/submission-table';
+import { submissions, users } from './components/sample-data';
 
-export default function Home() {
-  const data: Sample[] = [
-    {
-      status: 'submitted',
-      title: 'Sum of Digits',
-      difficulty: 1,
-      author: 'Suzuki',
-      published: '2025-01-04T10:00:00',
-    },
-    {
-      status: '',
-      title: '最大公約数',
-      difficulty: 1,
-      author: 'Satou',
-      published: '2025-01-05T10:00:00',
-    },
-    {
-      status: 'reviewed',
-      title: '最大の連続和',
-      difficulty: 2,
-      author: 'Takahashi',
-      published: '2024-12-31T10:00:00',
-    },
-    {
-      status: 'reviewed',
-      title: 'Reverse String',
-      difficulty: 1,
-      author: 'Suzuki',
-      published: '2025-01-04T10:00:00',
-    },
-    {
-      status: 'reviewed',
-      title: '3Sum',
-      difficulty: 2,
-      author: 'Satou',
-      published: '2024-12-31T10:00:00',
-    },
-    {
-      status: 'submitted',
-      title: 'N-クイーン問題',
-      difficulty: 3,
-      author: 'Itou',
-      published: '2024-10-04T10:00:00',
-    },
-    {
-      status: 'submitted',
-      title: '最短経路問題（ダイクストラ法）',
-      difficulty: 3,
-      author: 'Tanaka',
-      published: '2024-11-04T10:00:00',
-    },
-    {
-      status: 'submitted',
-      title: 'FizzBuzz',
-      difficulty: 1,
-      author: 'Satou',
-      published: '2025-01-05T15:00:00',
-    },
-    {
-      status: 'submitted',
-      title: '動的計画法によるナップサック問題',
-      difficulty: 3,
-      author: 'Takahashi',
-      published: '2024-12-31T10:00:00',
-    },
-  ];
+export default function Page() {
   return (
     <div className="px-4 py-2.5 flex gap-2.5 w-full">
       <div className="flex-grow flex flex-col items-start gap-4 w-full">
@@ -88,7 +24,7 @@ export default function Home() {
           </ToggleGroupItem>
         </ToggleGroup>
         <div className="w-full">
-          <SubmissionTable data={data} className="w-full" />
+          <SubmissionTable data={submissions} users={users} className="w-full" />
           <Link
             href="/submissions/1/edit"
             className="font-bold underline text-blue-600 visited:text-purple-600"
@@ -99,11 +35,11 @@ export default function Home() {
       </div>
       <div className="w-100 flex flex-col gap-3">
         <div className="border rounded-xl p-5 flex flex-col gap-2.5 bg-accent text-accent-foreground">
-          <h1 className="text-xl">Notifications</h1>
+          <h1 className="text-xl font-bold">Notifications</h1>
           通知がここに表示されます
         </div>
         <div className="border rounded-xl p-5 flex flex-col gap-2.5 bg-accent text-accent-foreground">
-          <h1 className="text-xl">Achievements</h1>
+          <h1 className="text-xl font-bold">Achievements</h1>
           実績がここに表示されます
         </div>
       </div>
