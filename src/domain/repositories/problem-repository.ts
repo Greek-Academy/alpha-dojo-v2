64e432649f5d064773e3e975dd17407d63a2e298
makoto yasuda <hare.v1v1enne@gmail.com>
2025-02-08 21:16:28 +0900

    delete: unused code.

--- src/domain/repositories/problem-repository.ts
+++ src/domain/repositories/problem-repository.ts
@@ -2,8 +2,4 @@ import { Problem } from '@/domain/entities/problem';
 
 export interface ProblemRepository {
   getProblems(): Promise<Problem[]>;
-  getProblemById(id: number): Promise<Problem | null>;
-  createProblem(input: Omit<Problem, 'id'>): Promise<Problem>;
-  updateProblem(problem: Problem): Promise<void>;
-  deleteProblem(id: number): Promise<void>;
 }
