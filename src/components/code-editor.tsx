932772b2ef9f5afe2a082a57cba2686d3680a088
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-11 08:49:06 +0000

    move: `lib/languages.ts` -> `src/lib/language.ts`

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Editor, { EditorProps } from '@monaco-editor/react';
-import { Language } from '../../lib/languages';
+import { Language } from '@/lib/languages';
 import Image from 'next/image';
 import Spinner from '@/../public/spinner.svg';
 import { cn } from '@/lib/utils';
