66090fcc02e276f0a2f665f2b17d1872c57bf8f5
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-12 06:34:21 +0000

    feat: ログイン時のアクセストークンを使用した Problems の取得

--- src/infrastructure/repositories/api-problem-repository.ts
+++ src/infrastructure/repositories/api-problem-repository.ts
@@ -1,18 +1,26 @@
 import { ProblemRepository } from '@/domain/repositories/problem-repository';
 import { Problem } from '@/domain/entities/problem';
 import { ProblemDTO } from '@/infrastructure/dto/problem-dto';
+import { cookies } from 'next/headers';
 
 const STRAPI_API_URL =
   process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337/api';
 
+// ライブラリとして共有すべき？
+export async function getAuthToken() {
+  const authToken = (await cookies()).get('jwt')?.value;
+  return authToken;
+}
+
 export class ApiProblemRepository implements ProblemRepository {
-  //TODO: Authorizationはログイン時のトークンを使用する
   async getProblems(): Promise<Problem[]> {
+    const authToken = await getAuthToken();
+
     const response = await fetch(`${STRAPI_API_URL}/problems`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_JWT}`,
+        Authorization: `Bearer ${authToken}`,
       },
     });
 
