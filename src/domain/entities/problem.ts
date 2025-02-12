0d34b63e1b27f46a87dc9343b2c0222c0f933808
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-12 08:12:27 +0000

    fix: `constrains` -> `constraints`

--- src/domain/entities/problem.ts
+++ src/domain/entities/problem.ts
@@ -6,7 +6,7 @@ export class Problem {
     public readonly title: string,
     public readonly description: string,
     public readonly difficulty: Difficulty,
-    public readonly constrains: string,
+    public readonly constraints: string,
     public readonly createdAt: Date,
     public readonly updatedAt: Date
   ) {}
