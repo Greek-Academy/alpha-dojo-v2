547e86fd7418703561a642d19dd7317580d5d256
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-13 08:36:52 +0000

    fix: shadcn Resizable を以前の仕様に合わせて仕様変更 (デフォルトで gap を配置。`hover`, `drag` 時に線を表示)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,9 +1,8 @@
-"use client"
+'use client';
 
-import { GripVertical } from "lucide-react"
-import * as ResizablePrimitive from "react-resizable-panels"
+import * as ResizablePrimitive from 'react-resizable-panels';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 const ResizablePanelGroup = ({
   className,
@@ -11,35 +10,69 @@ const ResizablePanelGroup = ({
 }: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
   <ResizablePrimitive.PanelGroup
     className={cn(
-      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
       className
     )}
     {...props}
   />
-)
+);
 
-const ResizablePanel = ResizablePrimitive.Panel
+const ResizablePanel = ResizablePrimitive.Panel;
+
+// const ResizableHandleVariants = cva(
+//   'bg-border-variant opacity-0 group-hover:opacity-100 transition-[opacity,width,height]',
+//   {
+//     variants: {
+//       direction: {
+//         horizontal: 'w-1 h-full group-active:w-0.5',
+//         vertical: 'h-1 w-full group-active:h-0.5',
+//       },
+//     },
+//     defaultVariants: {
+//       direction: 'horizontal',
+//     },
+//   }
+// );
 
 const ResizableHandle = ({
-  withHandle,
   className,
+  children,
+  gap = 10,
+  hitAreaMargins,
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
-  withHandle?: boolean
+  withHandle?: boolean;
+  gap?: number;
 }) => (
   <ResizablePrimitive.PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      // shadcn original:
+      // 'relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
+      'flex items-center justify-center focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 ' +
+        'group data-[panel-group-direction=horizontal]:!h-full data-[panel-group-direction=vertical]:!w-full',
       className
     )}
+    style={{
+      width: `${gap}px`,
+      height: `${gap}px`,
+      ...props.style,
+    }}
+    /** 余白を `gap` で指定しているため、追加のサイズ変更用の領域は不要
+     * (`react-resizable-panels` のデフォルトでは、`gap` は存在しない)
+     */
+    hitAreaMargins={{ coarse: 0, fine: 0, ...hitAreaMargins }}
     {...props}
   >
-    {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <GripVertical className="h-2.5 w-2.5" />
-      </div>
+    {children || (
+      <div
+        className={
+          'bg-border-variant opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-[opacity,width,height] ' +
+          'group-data-[panel-group-direction=horizontal]:w-1 group-data-[panel-group-direction=horizontal]:h-full group-active:group-data-[panel-group-direction=horizontal]:w-0.5 ' +
+          'group-data-[panel-group-direction=vertical]:h-1   group-data-[panel-group-direction=vertical]:w-full   group-active:group-data-[panel-group-direction=vertical]:h-0.5'
+        }
+      />
     )}
   </ResizablePrimitive.PanelResizeHandle>
-)
+);
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
