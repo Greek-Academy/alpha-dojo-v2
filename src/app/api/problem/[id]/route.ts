f027568863e5f2d6fbceb5a6ee4903282532691b
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-13 06:56:13 +0000

    fix: Dynamic Routes の GET の引数が誤っていたため、id を取得できていなかった

--- src/app/api/problem/[id]/route.ts
+++ src/app/api/problem/[id]/route.ts
@@ -6,13 +6,19 @@ import { getAuthToken } from '@/lib/get-auth-token';
 const problemRepository = new ApiProblemRepository();
 const problemUseCase = new ProblemUseCase(problemRepository);
 
-export async function GET({ params }: { params: Promise<{ id: number }> }) {
+export async function GET(
+  _request: Request,
+  { params }: { params: Promise<{ id: number }> }
+) {
   try {
     const authToken = await getAuthToken();
     if (!authToken)
       return NextResponse.json({ error: 'Failed to get jwt' }, { status: 401 });
 
-    const problem = await problemUseCase.getProblem((await params).id, authToken);
+    const problem = await problemUseCase.getProblem(
+      (await params).id,
+      authToken
+    );
     return NextResponse.json(problem, { status: 200 });
   } catch (_error) {
     return NextResponse.json(
