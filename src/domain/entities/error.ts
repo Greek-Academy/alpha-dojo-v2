d12a8518eb80296b8e404739b2f346be9db0b728
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-14 04:10:54 +0000

    add: Strapi からのレスポンスエラーを表す `ResponseError` クラスを作成

---
+++ src/domain/entities/error.ts
@@ -0,0 +1,16 @@
+export class ResponseError extends Error {
+  constructor(
+    /** レスポンスの HTTP ステータスコード */
+    public status: number,
+    /** `status`に対応するステータスメッセージ */
+    public statusText: string,
+    /** エラーの内容を表すオブジェクト */
+    public object?: object,
+    /** エラーの内容を表すメッセージ */
+    e?: string
+  ) {
+    super(e);
+    this.name = 'ResponseError';
+    this.message = `HTTP ${status}: ${e}`;
+  }
+}
