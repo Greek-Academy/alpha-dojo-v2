f7939256bcb8b8be9b29494134fe3769beaaf382
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-17 03:11:58 +0000

    fix: `ResponseError.message` を必須・`public` にした

--- src/domain/entities/error.ts
+++ src/domain/entities/error.ts
@@ -1,14 +1,14 @@
 export class ResponseError extends Error {
   constructor(
+    /** エラーの内容を表すメッセージ */
+    public message: string,
     /** レスポンスの HTTP ステータスコード */
     public status: number,
     /** `status`に対応するステータスメッセージ */
     public statusText: string,
-    /** エラーの内容を表すメッセージ */
-    e?: string
   ) {
-    super(e);
+    super(message);
     this.name = 'ResponseError';
-    this.message = `HTTP ${status}: ${e}`;
+    this.message = `HTTP ${status}: ${message}`;
   }
 }
