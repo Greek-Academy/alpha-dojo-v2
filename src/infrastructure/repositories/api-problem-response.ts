738a7344c6769fcf4056892862229cc7e79df141
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-17 10:26:06 +0000

    change: `problemsResponse` -> `StrapiProblems`

--- src/infrastructure/repositories/api-problem-response.ts
+++ src/infrastructure/repositories/api-problem-response.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { problemDTO } from "../dto/problem-dto";
 import { errorResponse } from "../dto/error";
 
-export const problemsResponse = z
+export const strapiProblems = z
   .object({
     data: z.array(problemDTO),
   })
   .or(errorResponse);
 
-export type ProblemsResponse = z.infer<typeof problemsResponse>;
+export type StrapiProblems = z.infer<typeof strapiProblems>;
