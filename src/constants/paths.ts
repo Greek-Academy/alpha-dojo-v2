18bd4f79599c810cd9cd18e2e9de3ace8ab2190e
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-21 15:48:21 +0000

    change: .env のいくつかの環境変数を必須にした `HOST`, `STRAPI_PUBLIC_URL`, `JUDGE_API_ENDPOINT`

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,15 +1,14 @@
 /** フロントエンドのドメイン
  *
- * 例: http://localhost:3000
+ * 例: localhost
  */
-export const HOST = process.env.HOST || 'localhost';
+export const HOST = process.env.HOST;
 
 /** Strapi の URL
  *
  * 例: http://localhost:1337
  */
-export const STRAPI_URL =
-  process.env.STRAPI_PUBLIC_URL || 'http://localhost:1337';
+export const STRAPI_URL = process.env.STRAPI_PUBLIC_URL;
 
 /** Strapi の API の URL
  *
@@ -21,5 +20,4 @@ export const STRAPI_API_URL = STRAPI_URL + '/api';
  *
  * 例: http://localhost:2358
  */
-export const JUDGE_API_ENDPOINT =
-  process.env.JUDGE_API_ENDPOINT || 'http://localhost:2358';
+export const JUDGE_API_ENDPOINT = process.env.JUDGE_API_ENDPOINT;
