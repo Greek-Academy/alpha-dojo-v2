fbb8005daeedbf0000930125c22fa8b07a9cc7e1
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-22 16:03:15 +0000

    fix: Test Case Entity に language プロパティは不要

--- src/domain/entities/test-case.ts
+++ src/domain/entities/test-case.ts
@@ -1,10 +1,7 @@
-import { SupportedLanguage } from './supported-language';
-
 export class TestCase {
   constructor(
     public readonly id: string,
     public readonly problemId: string,
-    public readonly language: SupportedLanguage,
     public readonly cmdArguments: string,
     public readonly expect: string,
     public readonly createdAt: Date,
