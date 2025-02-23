486eb9eeed94b65675773415808d703901795052
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-23 03:10:43 +0000

    add: 各コンテンツごとの DTO を作成

--- src/infrastructure/dto/problem-dto.ts
+++ src/infrastructure/dto/problem-dto.ts
@@ -1,15 +1,29 @@
-import { difficulty } from '@/domain/entities/problem';
 import { z } from 'zod';
+import { difficulty } from '@/domain/entities/problem';
+import { strapiCommonAttributesDTO, strapiCommonDTO } from './strapi-common-dto';
+import { validatorDTO } from './validator-dto';
+import { hintDTO } from './hint-dto';
+import { initialCodeDTO } from './initial-code-dto';
+import { testCaseDTO } from './test-case-dto'
 
-export const problemDTO = z.object({
-  id: z.number(),
-  attributes: z.object({
+export const problemDTO = strapiCommonDTO.extend({
+  attributes: strapiCommonAttributesDTO.extend({
     title: z.string(),
     description: z.string(),
     difficulty: difficulty,
     constraints: z.string(),
-    createdAt: z.string(),
-    updatedAt: z.string(),
+    validators: z.object({
+      data: z.array(validatorDTO)
+    }),
+    hints: z.object({
+      data: z.array(hintDTO)
+    }),
+    initial_codes: z.object({
+      data: z.array(initialCodeDTO)
+    }),
+    test_cases: z.object({
+      data: z.array(testCaseDTO)
+    })
   }),
 });
 
---
+++ src/infrastructure/dto/validator-dto.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { strapiCommonAttributesDTO, strapiCommonDTO } from './strapi-common-dto';
+import { ProblemDTO, problemDTO } from './problem-dto';
+import { languageDTO } from './lanuage-dto';
+
+const baseValidatorAttributesDTO = strapiCommonAttributesDTO.extend({
+  language_id: z
+    .object({
+      data: languageDTO,
+    })
+    .optional(),
+  code: z.string(),
+})
+
+const baseValidatorDTO = strapiCommonDTO.extend({
+  attributes: baseValidatorAttributesDTO,
+});
+
+export type ValidatorDTO = z.infer<typeof baseValidatorDTO> & {
+  attributes: {
+    problem_id?: {
+      data: ProblemDTO;
+    };
+  };
+};
+
+export const validatorDTO: z.ZodType<ValidatorDTO> = baseValidatorDTO.extend({
+  attributes: baseValidatorAttributesDTO.extend({
+    problem_id: z
+      .object({
+        data: z.lazy(() => problemDTO),
+      })
+      .optional(),
+  }),
+});
