c801ce4ded9631a0a766931746562458a01aa31a
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-02-23 03:13:21 +0000

    fix: Entity / DTO の修正に合わせて `problem-repository` を修正

--- src/infrastructure/problem/problem-repository.ts
+++ src/infrastructure/problem/problem-repository.ts
@@ -6,21 +6,90 @@ import { WithJson, withJson } from '../infra-utils';
 import { StrapiError } from '../strapi-error';
 import { strapiProblems } from './problem-response';
 import { errorResponse } from '../dto/error';
+import qs from 'qs';
+import { supportedLanguageEnum } from '@/domain/entities/supported-language';
+import { newHintFromDTO } from '../hint/hint-repository';
+import { newInitialCodeFromDTO } from '../initial-code/initial-code-repository';
+import { newTestCaseFromDTO } from '../test-case/test-case-repository';
+import { newValidatorFromDTO } from '../validator/validator-repository';
+import { ProblemDTO } from '../dto/problem-dto';
 
 const STRAPI_API_URL =
   (process.env.STRAPI_PUBLIC_URL || 'http://localhost:1337') + '/api';
+
+export const newProblemFromDTO = (problem: ProblemDTO) => {
+  return new Problem(
+    problem.id.toString(),
+    problem.attributes.title,
+    problem.attributes.description,
+    problem.attributes.difficulty,
+    problem.attributes.constraints,
+    problem.attributes.hints.data.map((hint) =>
+      newHintFromDTO(hint, problem.id.toString())
+    ),
+    Object.fromEntries(
+      supportedLanguageEnum.map((language) => {
+        const initialCode = problem.attributes.initial_codes.data.find(
+          (initialCode) =>
+            initialCode.attributes.language_id?.data.attributes.name ===
+            language
+        );
+        return [
+          language,
+          initialCode &&
+            newInitialCodeFromDTO(initialCode, problem.id.toString(), language),
+        ];
+      })
+    ),
+    problem.attributes.test_cases.data.map((testCase) =>
+      newTestCaseFromDTO(testCase, problem.id.toString())
+    ),
+    // Initial Code と Validator のコードが全く同じなので改善の余地あり
+    Object.fromEntries(
+      supportedLanguageEnum.map((language) => {
+        const validator = problem.attributes.validators.data.find(
+          (validator) =>
+            validator.attributes.language_id?.data.attributes.name === language
+        );
+        return [
+          language,
+          validator &&
+            newValidatorFromDTO(validator, problem.id.toString(), language),
+        ];
+      })
+    ),
+    new Date(problem.attributes.createdAt),
+    new Date(problem.attributes.updatedAt)
+  );
+};
 
 export class ApiProblemRepository implements ProblemRepository {
   //TODO: Authorizationはログイン時のトークンを使用する
   getProblems = () =>
     ResultAsync.fromPromise(
-      fetch(`${STRAPI_API_URL}/problems`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_JWT}`,
-        },
-      }),
+      fetch(
+        `${STRAPI_API_URL}/problems?${qs.stringify(
+          {
+            // Strapi の Relations をレスポンスに含める
+            populate: [
+              'validators.language_id',
+              'hints',
+              'initial_codes.language_id',
+              'test_cases',
+            ],
+          },
+          {
+            encodeValuesOnly: true, // prettify URL
+          }
+        )}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_JWT}`,
+          },
+        }
+      ),
       normalizeError
     )
       .andThen(withJson)
@@ -40,20 +109,7 @@ export class ApiProblemRepository implements ProblemRepository {
             )
       )
       .andThen((problems) =>
-        ok(
-          problems.map(
-            (problem) =>
-              new Problem(
-                problem.id,
-                problem.attributes.title,
-                problem.attributes.description,
-                problem.attributes.difficulty,
-                problem.attributes.constraints,
-                new Date(problem.attributes.createdAt),
-                new Date(problem.attributes.updatedAt)
-              )
-          )
-        )
+        ok(problems.map((problem) => newProblemFromDTO(problem)))
       )
       .mapErr((err) => err.toResponseError());
 }
