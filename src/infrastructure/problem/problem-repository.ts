285addc3f55ee2b929c408b50a28b45ea64adb20
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-02 02:35:36 +0000

    fix: Strapi から Problem を取得するコードを修正 Problem Entity の変更に合わせる

--- src/infrastructure/problem/problem-repository.ts
+++ src/infrastructure/problem/problem-repository.ts
@@ -6,12 +6,6 @@ import { WithJson, withJson } from '../infra-utils';
 import { StrapiError } from '../strapi-error';
 import { strapiProblems } from './problem-response';
 import { errorResponse } from '../dto/error';
-import qs from 'qs';
-import { supportedLanguageEnum } from '@/domain/entities/supported-language';
-import { newHintFromDTO } from '../hint/hint-repository';
-import { newInitialCodeFromDTO } from '../initial-code/initial-code-repository';
-import { newTestCaseFromDTO } from '../test-case/test-case-repository';
-import { newValidatorFromDTO } from '../validator/validator-repository';
 import { ProblemDTO } from '../dto/problem-dto';
 
 const STRAPI_API_URL =
@@ -24,40 +18,6 @@ export const newProblemFromDTO = (problem: ProblemDTO) => {
     problem.attributes.description,
     problem.attributes.difficulty,
     problem.attributes.constraints,
-    problem.attributes.hints?.data.map((hint) =>
-      newHintFromDTO(hint, problem.id.toString())
-    ) ?? [],
-    Object.fromEntries(
-      supportedLanguageEnum.map((language) => {
-        const initialCode = problem.attributes.initial_codes?.data.find(
-          (initialCode) =>
-            initialCode.attributes.language_id?.data.attributes.name ===
-            language
-        );
-        return [
-          language,
-          initialCode &&
-            newInitialCodeFromDTO(initialCode, problem.id.toString(), language),
-        ];
-      })
-    ),
-    problem.attributes.test_cases?.data.map((testCase) =>
-      newTestCaseFromDTO(testCase, problem.id.toString())
-    ) ?? [],
-    // Initial Code と Validator のコードが全く同じなので改善の余地あり
-    Object.fromEntries(
-      supportedLanguageEnum.map((language) => {
-        const validator = problem.attributes.validators?.data.find(
-          (validator) =>
-            validator.attributes.language_id?.data.attributes.name === language
-        );
-        return [
-          language,
-          validator &&
-            newValidatorFromDTO(validator, problem.id.toString(), language),
-        ];
-      })
-    ),
     new Date(problem.attributes.createdAt),
     new Date(problem.attributes.updatedAt)
   );
@@ -67,29 +27,13 @@ export class ApiProblemRepository implements ProblemRepository {
   //TODO: Authorizationはログイン時のトークンを使用する
   getProblems = () =>
     ResultAsync.fromPromise(
-      fetch(
-        `${STRAPI_API_URL}/problems?${qs.stringify(
-          {
-            // Strapi の Relations をレスポンスに含める
-            populate: [
-              'validators.language_id',
-              'hints',
-              'initial_codes.language_id',
-              'test_cases',
-            ],
-          },
-          {
-            encodeValuesOnly: true, // prettify URL
-          }
-        )}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_JWT}`,
-          },
-        }
-      ),
+      fetch(`${STRAPI_API_URL}/problems`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_JWT}`,
+        },
+      }),
       normalizeError
     )
       .andThen(withJson)
