2dbde590efb9ed49a9a2e7e9a3531abf0f92eca3
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-03 09:43:55 +0000

    change: `path.ts` の値を必須にした 型が `string | undefined` だと扱いにくい

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -2,13 +2,14 @@
  *
  * 例: localhost
  */
-export const HOST = process.env.HOST;
+export const HOST = process.env.HOST || 'localhost';
 
 /** Strapi の URL
  *
  * 例: http://localhost:1337
  */
-export const STRAPI_URL = process.env.STRAPI_PUBLIC_URL;
+export const STRAPI_URL =
+  process.env.STRAPI_PUBLIC_URL || 'http://localhost:1337';
 
 /** Strapi の API の URL
  *
@@ -20,4 +21,5 @@ export const STRAPI_API_URL = STRAPI_URL + '/api';
  *
  * 例: http://localhost:2358
  */
-export const JUDGE_API_ENDPOINT = process.env.JUDGE_API_ENDPOINT;
+export const JUDGE_API_ENDPOINT =
+  process.env.JUDGE_API_ENDPOINT || 'http://localhost:2358';
