c4212eb6c353da1ac83cf8a1995500e06036c09d
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-03 09:55:27 +0000

    add: `constants/env` に EnvValue を定義

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,32 @@
+import { HOST } from './paths';
+
 export const NODE_ENV = process.env.NODE_ENV;
 export const JUDGE_API_KEY = process.env.JUDGE_API_KEY ?? '';
+
+export interface EnvValue {
+  host: string;
+  secure: boolean;
+  port: number;
+}
+
+export function getEnvValues(env: NodeJS.ProcessEnv['NODE_ENV']): EnvValue {
+  switch (env) {
+    case 'production': {
+      return {
+        host: HOST,
+        secure: true,
+        port: 1337,
+      };
+    }
+
+    default: {
+      return {
+        host: HOST,
+        secure: false,
+        port: 1337,
+      };
+    }
+  }
+}
+
+export const envValues = getEnvValues(NODE_ENV);
