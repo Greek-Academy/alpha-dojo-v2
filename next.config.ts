ecec434dd248a1fd40e3f61201bb427be0f83fae
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-23 15:02:35 +0000

    fix: env で `HOST` に指定した URL からも Next.js の開発サーバーにアクセス可能に オンライン開発環境 (Project IDX 等) では、プレビューをする際に外部からアクセスするため、この設定が必要

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+import { HOST } from '@/constants/paths';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // オンライン開発環境 (Project IDX 等) では、プレビューをする際に外部からアクセスするため、この設定が必要
+  allowedDevOrigins: [HOST]
 };
 
 export default nextConfig;
