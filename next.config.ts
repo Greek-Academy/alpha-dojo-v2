5a0a83a19a27d7b7be242d1410affbfb6ecd3a07
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-23 15:12:03 +0000

    chore: Prettier による修正

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // オンライン開発環境 (Project IDX 等) では、プレビューをする際に外部からアクセスするため、この設定が必要
-  allowedDevOrigins: [HOST]
+  allowedDevOrigins: [HOST],
 };
 
 export default nextConfig;
