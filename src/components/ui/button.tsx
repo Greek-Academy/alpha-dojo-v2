d10470b864a381ba0719edbdbab849a2062078e8
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-25 12:39:04 +0000

    update: shadcn Button * shadcn の最新コードとの同期 * TailwindCSS v4 への対応 * React 19 への対応 * デザインの調整

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,30 +5,36 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  // shadcn original:
-  // "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 ' +
-    'rounded-full ' +
-    // 子要素の Material Symbols のフォントサイズは 18px が既定
-    '[&>_]:[&_[class^="material-symbols-"]]:text-[18px]',
+  cn(
+    // "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-label-large transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-secondary focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+    'rounded-full cursor-pointer transition overflow-hidden ring-offset-2',
+    /* State layer */ 'relative after:absolute after:inset-0 after:opacity-0 after:transition-opacity',
+    /* Hovered */ 'hover:after:opacity-8',
+    /* Focused */ 'focus-visible:after:opacity-10',
+    /* Pressed */ 'active:after:opacity-10 active:drop-shadow-none',
+    /* Disabled */ 'disabled:text-foreground/38',
+    /* Icon */ '[&>[class*="material-symbols-"]]:first:text-[18px]!'
+  ),
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'hover:drop-shadow-sm bg-primary text-primary-foreground after:bg-primary-foreground disabled:bg-foreground/12',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'hover:drop-shadow-sm bg-destructive text-white after:bg-primary-foreground focus-visible:ring-destructive disabled:bg-foreground/12',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'border text-primary after:bg-primary disabled:border-foreground/12',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+          'hover:drop-shadow-sm bg-secondary text-secondary-foreground after:bg-secondary-foreground disabled:bg-foreground/12',
+        ghost: 'text-primary after:bg-primary',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: 'px-6 py-2.5 [&>[class*="material-symbols-"]]:first:-ml-2',
+        sm: 'px-3 py-2.5 has-[[class*="material-symbols-"]]:first:pr-4',
+        lg: 'rounded-md px-6',
+        icon: 'size-10',
       },
     },
     defaultVariants: {
@@ -38,26 +44,27 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
-
 /** SVG アイコンのサイズは自動的に 1rem に縮小されるので、
  *  強制的に変更したい場合は className に important modifier をつける。 */
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
-Button.displayName = 'Button';
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
 
 export { Button, buttonVariants };
