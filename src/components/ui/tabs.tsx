601fe834329ad2e29ff5be386463fecd4f1a88df
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-03-26 08:00:08 +0000

    update: shadcn Tabs * shadcn の最新コードとの同期 * TailwindCSS v4 への対応 * React 19 への対応 * 一部デザインの修正

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -5,79 +5,77 @@ import * as TabsPrimitive from '@radix-ui/react-tabs';
 
 import { cn } from '@/lib/utils';
 
-const Tabs = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Root
-    ref={ref}
-    className={cn(
-      'rounded-xl overflow-hidden border ' + 'flex flex-col',
-      className
-    )}
-    {...props}
-  />
-));
-Tabs.displayName = TabsPrimitive.Root.displayName;
-
-const TabsList = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      // shadcn original:
-      // "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
-      'inline-flex h-auto items-center justify-center bg-neutral-200 p-1 text-muted-foreground',
-      'gap-1 flex justify-start text-primary overflow-x-auto shrink-0',
-      className
-    )}
-    {...props}
-  />
-));
-TabsList.displayName = TabsPrimitive.List.displayName;
-
-const TabsTrigger = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <>
-    <TabsPrimitive.Trigger
-      ref={ref}
+function Tabs({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
       className={cn(
-        // shadcn original:
-        // "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-xs",
-        'inline-flex items-center justify-center whitespace-nowrap rounded-lg px-4 py-1.5 text-label-medium-prominent ring-offset-background transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-state-layers-primary/12 data-[state=active]:shadow-xs',
-        'gap-2 w-auto! hover:bg-state-layers-primary/8',
-        // 子要素の Material Symbols のフォントサイズは 18px が既定
-        '[&>_]:[&_[class^="material-symbols-"]]:text-[18px]',
+        // "flex flex-col gap-2",
+        'flex flex-col',
+        'rounded-xl overflow-hidden border',
         className
       )}
       {...props}
     />
-    {/* 縦の区切り線 */}
-    <div className="bg-neutral-400 w-px h-5 last:hidden shrink-0" />
-  </>
-));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+  );
+}
 
-const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      // shadcn original:
-      // "mt-2 ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      'ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-      'grow overflow-auto',
-      className
-    )}
-    {...props}
-  />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+function TabsList({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+        // 'bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]',
+        'bg-neutral-200 text-muted-foreground inline-flex h-auto items-center justify-start p-1',
+        'gap-1 flex text-primary overflow-x-auto shrink-0',
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function TabsTrigger({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <>
+      <TabsPrimitive.Trigger
+        data-slot="tabs-trigger"
+        className={cn(
+          // "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+          "data-[state=active]:bg-state-layers-primary/12 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-label-medium-prominent inline-flex h-[calc(100%-1px)] items-center justify-center gap-2 rounded-lg border border-transparent px-4 py-1.5 whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+          'hover:bg-state-layers-primary/8 transition-colors',
+          // 子要素の Material Symbols のフォントサイズは 18px が既定
+          '[&>_]:[&_[class^="material-symbols-"]]:text-[18px]',
+          className
+        )}
+        {...props}
+      />
+      {/* 縦の区切り線 */}
+      <div className="bg-neutral-400 w-px h-5 last:hidden shrink-0" />
+    </>
+  );
+}
+
+function TabsContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn('grow overflow-auto', className)}
+      {...props}
+    />
+  );
+}
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
