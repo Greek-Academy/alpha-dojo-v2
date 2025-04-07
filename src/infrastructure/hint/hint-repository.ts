557d4d1b584175135e73909614e817898337453e
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-04-07 22:43:08 +0900

    feat: ApiSubmissionRepository を実装し、Hint を取得するメソッドを追加

--- src/infrastructure/hint/hint-repository.ts
+++ src/infrastructure/hint/hint-repository.ts
@@ -1,5 +1,11 @@
 import { Hint } from '@/domain/entities/hint';
-import { HintDTO } from './hint-response';
+import { hintDTO, HintDTO } from './hint-response';
+import { fetchStrapiData } from '../strapi/strapi-utils';
+import { ok } from 'neverthrow';
+import {
+  HintFilters,
+  HintRepository,
+} from '@/domain/repositories/hint-repository';
 
 export const newHintFromDTO = (hint: HintDTO) => {
   return new Hint(
@@ -10,3 +16,37 @@ export const newHintFromDTO = (hint: HintDTO) => {
     new Date(hint.attributes.updatedAt)
   );
 };
+
+const hintEndpoint = '/hints';
+
+export class ApiSubmissionRepository implements HintRepository {
+  constructor(private readonly authToken?: string) {}
+
+  getHintById = (id: string) =>
+    fetchStrapiData<HintDTO>(
+      `${hintEndpoint}/${id}`,
+      hintDTO,
+      {},
+      this.authToken
+    )
+      .andThen((hint) => ok(newHintFromDTO(hint)))
+      .mapErr((err) => err.toResponseError());
+
+  getHints = (filters?: HintFilters) =>
+    fetchStrapiData<HintDTO[]>(
+      hintEndpoint,
+      hintDTO.array(),
+      {
+        filters: {
+          problem_id: {
+            id: {
+              $eq: Number(filters?.problemId),
+            },
+          },
+        },
+      },
+      this.authToken
+    )
+      .andThen((hints) => ok(hints.map((hint) => newHintFromDTO(hint))))
+      .mapErr((err) => err.toResponseError());
+}
