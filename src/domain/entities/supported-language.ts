fc0d9819e2ce326e5f9ce9c8a90180c5ad3adcb0
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-04-13 11:03:07 +0900

    feat: Language クラスを追加し、サポートされる言語を定義

--- src/domain/entities/supported-language.ts
+++ src/domain/entities/supported-language.ts
@@ -8,3 +8,30 @@ export const supportedLanguageEnum = [
 /** Zod */
 export const supportedLanguage = z.enum(supportedLanguageEnum);
 export type SupportedLanguage = z.infer<typeof supportedLanguage>;
+
+export class Language {
+  public readonly label: string;
+  public readonly order: number;
+
+  constructor(
+    public readonly id: SupportedLanguage,
+    options: {
+      label: string;
+      order: number;
+    }
+  ) {
+    this.label = options.label;
+    this.order = options.order;
+    Object.freeze(this);
+  }
+}
+
+export const typescript = new Language('TYPESCRIPT', {
+  label: 'TypeScript',
+  order: 1,
+});
+
+export const python = new Language('PYTHON', {
+  label: 'Python',
+  order: 2,
+});
