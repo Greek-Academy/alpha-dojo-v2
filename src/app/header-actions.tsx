10185ed533c1aae52b05b760dc5fff228ef6e471
iusok3386 <109420050+iusok3386@users.noreply.github.com>
2025-05-03 23:54:41 +0900

    feat: ヘッダーの提出ボタンの処理を実装

--- src/app/header-actions.tsx
+++ src/app/header-actions.tsx
@@ -2,8 +2,12 @@
 
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 import { Play, Upload } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/hooks/react-redux';
+import { submitSubmission } from '@/lib/features/submission/submission-slice';
+import { SubmissionToCreate } from '@/domain/entities/submission';
+import { languageKeyToLanguage } from '@/domain/repositories/language-repository';
 
 export type HeaderButtonsProps = React.ButtonHTMLAttributes<HTMLDivElement>;
 
@@ -11,12 +15,28 @@ export function HeaderButtons(props: HeaderButtonsProps) {
   const pathname = usePathname();
   const buttonEnabledPages = /^\/submissions\/\w+\/edit$/;
 
+  const params = useParams<{ id: string }>();
+
+  const user = useAppSelector((state) => state.user);
+  const submission = useAppSelector((state) => state.submission.newSubmission);
+  const dispatch = useAppDispatch();
+
   function runButtonHandler() {
     alert('WIP');
   }
 
   function submitButtonHandler() {
-    alert('WIP');
+    if (user.id === undefined) {
+      // FIXME: エラー時の処理
+      return;
+    }
+    const submissionToCreate = new SubmissionToCreate(
+      user.id,
+      params.id,
+      languageKeyToLanguage(submission.codeLanguageKey),
+      submission.codeText
+    );
+    dispatch(submitSubmission(submissionToCreate));
   }
 
   return (
